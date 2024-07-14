fcd20f1a6c8ec6459539ff05235ba10467127725
null <null>
2024-07-14 17:10:14 +0800

    part 1a completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,54 @@
-const App = () => {
-  console.log('Hello from component')
+const Header = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1>Course: {props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      Part: {props.part}, Exercises: {props.exercises}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      Total Exercise: {props.totalExercises}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} 
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total totalExercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
