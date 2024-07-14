63bf3109a5c08497d1a8b5bd71da9f28843e590b
null <null>
2024-07-14 23:02:40 +0800

    Added general for general coding and testing

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,9 +10,9 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        <Part part={props.part1} exercises={props.exercises1}/>
-        <Part part={props.part2} exercises={props.exercises2}/>
-        <Part part={props.part3} exercises={props.exercises3}/>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
       </p>
     </div>
   )
@@ -21,7 +21,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <div>
-      Part: {props.part}, Exercises: {props.exercises}
+      Part: {props.part.name}, Exercises: {props.part.exercises}
     </div>
   )
 }
@@ -29,26 +29,35 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      Total Exercise: {props.totalExercises}
+      Total Exercise: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} 
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total totalExercises={exercises1 + exercises2 + exercises3}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
