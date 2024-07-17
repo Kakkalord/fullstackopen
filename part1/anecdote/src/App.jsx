d8f165afbdbe06c6dcc7ff458cc837f1979c4bdd
Kakkalord <clarenceywang@gmail.com>
2024-07-17 15:38:25 +0800

    Completed part 1 and exercises 1.6 - 1.14

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const Display = ({anecdotes, points}) => {
+  return (
+    <div>
+      <p>{anecdotes}</p>
+      <p>has {points} points</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const handleNextAnecdote = () => {
+    return setSelected(getRandomInt(8))
+  }
+
+  const handleVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Uint8Array(8))
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const indexOfLargestValue = points.reduce((maxIndex, currentValue, currentIndex, array) => currentValue > array[maxIndex] ? currentIndex : maxIndex, 0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Display anecdotes={anecdotes[selected]} points={points[selected]} />
+      <Button onClick={handleVote} text='vote' />
+      <Button onClick={handleNextAnecdote} text='next anecdote' />
+      <h1>Anecdote with most votes</h1>
+      <Display anecdotes={anecdotes[indexOfLargestValue]} points={points[indexOfLargestValue]} />
+    </div>
   )
 }
 
