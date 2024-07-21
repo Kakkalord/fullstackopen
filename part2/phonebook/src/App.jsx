e9d418a04dda328d3ceffabf3b94313d7d4232c5
Kakkalord <clarenceywang@gmail.com>
2024-07-21 21:45:19 +0800

    2.8 completed

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  // when form submitted
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    
+    // create new object to hold the person
+    const personObject = {
+      name: newName,
+    }
+
+    // check if name is already present
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+
+  // handle changes made to event.target (input)
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
