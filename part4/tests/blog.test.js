b54317e20871ecbd79ec247296ee44797d2cf324
Kakkalord <clarenceywang@gmail.com>
2024-08-13 13:48:31 +0800

    Completed 4.5. Return to complete 4.6/4.7 when rd

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -1,12 +1,12 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
-const { dummy, totalLikes } = require('../utils/list_helper')
+const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
     const blogs = []
     
-    assert.strictEqual(dummy(blogs), 1)
+    assert.strictEqual(listHelper.dummy(blogs), 1)
 })
 
 describe('totalLikes', () => {
@@ -38,7 +38,7 @@ describe('totalLikes', () => {
             }
         ]
 
-        assert.strictEqual(totalLikes(blogs), 0)
+        assert.strictEqual(listHelper.totalLikes(blogs), 0)
     })
     
     test('total likes of list of 3 blogs = 25', () => {
@@ -69,7 +69,55 @@ describe('totalLikes', () => {
             }
         ]
 
-        assert.strictEqual(totalLikes(blogs), 25)
+        assert.strictEqual(listHelper.totalLikes(blogs), 25)
+    })
+
+describe('favouriteBlog', () => {
+    test('favourite blog with list of 3 blogs', () => {
+        const blogs = [
+            {
+              _id: "5a422a851b54a676234d17f7",
+              title: "React patterns",
+              author: "Michael Chan",
+              url: "https://reactpatterns.com/",
+              likes: 7,
+              __v: 0
+            },
+            {
+              _id: "5a422aa71b54a676234d17f8",
+              title: "Go To Statement Considered Harmful",
+              author: "Edsger W. Dijkstra",
+              url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+              likes: 5,
+              __v: 0
+            },
+            {
+              _id: "5a422b3a1b54a676234d17f9",
+              title: "Canonical string reduction",
+              author: "Edsger W. Dijkstra",
+              url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+              likes: 12,
+              __v: 0
+            }
+        ]
+
+        assert.deepEqual(listHelper.favouriteBlog(blogs), {title: "Canonical string reduction", author: "Edsger W. Dijkstra", likes: 12});
+    })
+
+    test('favourite blog with 1 blog with 0 likes', () => {
+        const blogs = [
+            {
+                _id: "5a422ba71b54a676234d17fb",
+                title: "TDD harms architecture",
+                author: "Robert C. Martin",
+                url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+                likes: 0,
+                __v: 0
+              },
+            ]
+
+        assert.deepEqual(listHelper.favouriteBlog(blogs), {title: "TDD harms architecture", author: "Robert C. Martin", likes: 0})
+        })
     })
 })
 
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0);
 }
 
+const favouriteBlog = (blogs) => {
+    const reducer = (blog1, blog2) => {
+        return (blog1.likes > blog2.likes) ? blog1: blog2;
+    }
+
+    const blogWithMostLikes = blogs.reduce(reducer, blogs[0]);
+    return {
+        title: blogWithMostLikes.title,
+        author: blogWithMostLikes.author,
+        likes: blogWithMostLikes.likes
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favouriteBlog,
 }
