d76a80ab655d95492e97d019b9390062d4121091
Kakkalord <clarenceywang@gmail.com>
2024-08-13 11:56:44 +0800

    completed 4.4

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -1,11 +1,75 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
-const dummy = require('../utils/list_helper').dummy
+const { dummy, totalLikes } = require('../utils/list_helper')
 
 test('dummy returns one', () => {
     const blogs = []
     
     assert.strictEqual(dummy(blogs), 1)
+})
+
+describe('totalLikes', () => {
+    test('total likes of list of 3 blogs = 0', () => {
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 0,
+                __v: 0
+            }, 
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 0,
+                __v: 0
+            }, 
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 0,
+                __v: 0
+            }
+        ]
+
+        assert.strictEqual(totalLikes(blogs), 0)
+    })
+    
+    test('total likes of list of 3 blogs = 25', () => {
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 5,
+                __v: 0
+            }, 
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 10,
+                __v: 0
+            }, 
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 10,
+                __v: 0
+            }
+        ]
+
+        assert.strictEqual(totalLikes(blogs), 25)
+    })
 })
 
