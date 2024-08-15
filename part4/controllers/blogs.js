e9742d9dfc904d6a7b72e067830b83d6be78ba17
Kakkalord <clarenceywang@gmail.com>
2024-08-15 12:50:02 +0800

    completing 4.14.

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', (request, response) => {
@@ -33,6 +34,17 @@ blogRouter.post('/api/blogs', (request, response) => {
 blogRouter.delete('/api/blogs/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
+})
+
+blogRouter.put('api/blogs/:id', async (request, response) => {
+  const blog = request.body
+
+  const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  if (updatedBlog) {
+    response.json(updatedBlog)
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogRouter
